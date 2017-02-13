a64a06934919e62466bb2660a0f04cb58fb5d1c1
Alejandra Agreda <agos.alejandramaria@gmail.com>
2017-02-13 00:17:27 -0500

    node_modules were added

---
+++ keys.js
@@ -0,0 +1,9 @@
+//Twitter API keys
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 's2eazgVBB80uPe4ifs4DG9z7B',
+  consumer_secret: '	KkdtkhfyeJWvc2zsaqd3HrHyCzDz6R0l27SMB2PcgXZaKKzWEw',
+  access_token_key: '78189287-l1OG5PDMaiakFqzyoYBklHZOns86WiaXqex8wp9N6',
+  access_token_secret: '8k8LBEZEHzasXY6bYdfuUdPbBULmavg8FigF6wwFb6PWJ',
+}
